362644a040b0f08824902354e159a9c2dc51397e
ShruS02 <ss2896@srmist.edu.in>
2026-04-06 14:04:53 +0530

    usecase17

--- TrainManagementApp/TrainConsistApp.java
+++ TrainManagementApp/TrainConsistApp.java
@@ -1,19 +1,9 @@
+import java.util.Arrays;
+
 public class TrainConsistApp {
     public static void main(String[] args) {
-        int[] capacities = {72, 56, 24, 70, 60};
-
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        String[] bogieNames = {"Sleeper","AC Chair","First Class","General","Luxury"};
+        Arrays.sort(bogieNames);
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
