d3fd17f612c652433d8ff5600f69122758c5ee50
ShruS02 <ss2896@srmist.edu.in>
2026-04-06 14:07:44 +0530

    usecase18

--- TrainManagementApp/TrainConsistApp.java
+++ TrainManagementApp/TrainConsistApp.java
@@ -1,9 +1,20 @@
-import java.util.Arrays;
-
 public class TrainConsistApp {
     public static void main(String[] args) {
-        String[] bogieNames = {"Sleeper","AC Chair","First Class","General","Luxury"};
-        Arrays.sort(bogieNames);
-        System.out.println(Arrays.toString(bogieNames));
+        String[] bogieIds = {"BG101","BG205","BG309","BG412","BG550"};
+        String searchKey = "BG309";
+        boolean found = false;
+
+        for (int i = 0; i < bogieIds.length; i++) {
+            if (bogieIds[i].equals(searchKey)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Bogie ID found");
+        } else {
+            System.out.println("Bogie ID not found");
+        }
     }
 }
